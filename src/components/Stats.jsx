4833d51f85487c23987bf092b734669f757eaaaa
Justin OConnell <itsjustinoconnell@gmail.com>
2024-01-12 14:37:13 +0000

    created reusable stat component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,21 @@
 function Stats() {
-  return <section className="stats">Stats</section>;
+  return (
+    <section className="stats">
+      <Stat text="words" number={0} />
+      <Stat text="characters" number={0} />
+      <Stat text="twitter" number={280} />
+      <Stat text="tweets" number={0} />
+    </section>
+  );
+}
+
+function Stat({ text, number }) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{number}</span>
+      <h2 className="second-heading">{text}</h2>
+    </section>
+  );
 }
 
 export default Stats;
