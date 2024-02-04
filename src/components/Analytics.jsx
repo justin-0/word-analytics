d52f0ac66fe9c3b762497e7caa21df97123e95bd
Justin OConnell <itsjustinoconnell@gmail.com>
2024-02-04 15:38:02 +0000

    feat: localstorage functionality added

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AnalyticsContainer from "./AnalyticsContainer";
 import Stats from "./Stats";
 import TextArea from "./TextArea";
 
 function Analytics() {
-  const [chars, setChars] = useState("");
+  const checkStorage = localStorage.getItem("text");
+  const initStorage = () => {
+    if (checkStorage) {
+      return JSON.parse(checkStorage);
+    } else {
+      return "";
+    }
+  };
+
+  const [chars, setChars] = useState(initStorage());
+
+  useEffect(() => {
+    localStorage.setItem("text", JSON.stringify(chars));
+  }, [chars]);
 
   return (
     <>
